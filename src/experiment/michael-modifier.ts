1cd48e9395dea9720a7580d0aa6ef1fe661324c0
Marubio <michael@verafirma.com>
2024-10-23 17:34:28 -0500

    Update michael-modifier.ts

--- src/experiment/michael-modifier.ts
+++ src/experiment/michael-modifier.ts
@@ -1,9 +1,22 @@
 import { FablerInputModifier } from "../core/fabler-input-modifier";
 
+// Dummy data table for input lookup
+const dataTable: { [key: string]: string } = {
+  "look around": "look",
+  "scooby": "look"
+};
+
 export class MichaelModifier implements FablerInputModifier {
   public async modifyInput(userInput: string): Promise<string> {
-    let rval: string = userInput + 'x';
-    return rval;
+	  // Check input against data table
+    if (dataTable[userInput]) {
+      return dataTable[userInput];
+    }
+
+    // Return the original input if no match is found
+    return userInput;
+ //   let rval: string = userInput + 'x';
+ //   return rval;
   }
 
 }
